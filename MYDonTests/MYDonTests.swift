c2385da1488833a1d68df3bcf4179c9ace54dcad
DongDDD <jobs@jobsdemacbook-pro.local>
2015-11-03 15:20:53 -0800

    Initial Commit

---
+++ MYDonTests/MYDonTests.swift
@@ -0,0 +1,36 @@
+//
+//  MYDonTests.swift
+//  MYDonTests
+//
+//  Created by jobs on 15/11/3.
+//  Copyright © 2015年 jobs. All rights reserved.
+//
+
+import XCTest
+@testable import MYDon
+
+class MYDonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
